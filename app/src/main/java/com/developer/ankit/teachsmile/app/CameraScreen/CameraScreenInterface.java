a4383f8e310c20f6039d12f5bc310c67f71a7fb0
Ankit Sharma <ankitsharma.nov11@gmail.com>
2017-05-01 19:58:58 -0400

    Added camera screen activity

---
+++ app/src/main/java/com/developer/ankit/teachsmile/app/CameraScreen/CameraScreenInterface.java
@@ -0,0 +1,15 @@
+package com.developer.ankit.teachsmile.app.CameraScreen;
+
+/**
+ * Created by ankit on 4/29/17.
+ */
+
+public interface CameraScreenInterface {
+    interface View {
+
+    }
+
+    interface Presenter {
+
+    }
+}
---
+++ app/src/main/java/com/developer/ankit/teachsmile/app/CameraScreen/CameraScreenPresenter.java
@@ -0,0 +1,8 @@
+package com.developer.ankit.teachsmile.app.CameraScreen;
+
+/**
+ * Created by ankit on 4/29/17.
+ */
+
+public class CameraScreenPresenter {
+}
