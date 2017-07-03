bf098869336cd4d2ef41eb5a265b681af2dcd593
Ankit Sharma <ankit@intrepid.io>
2017-07-03 14:44:15 -0400

    Added Widget service class

--- app/src/main/java/com/developer/ankit/teachsmile/app/Widget/TeachSmileWidgetService.java
+++ app/src/main/java/com/developer/ankit/teachsmile/app/Widget/TeachSmileWidgetService.java
@@ -1,8 +1,65 @@
 package com.developer.ankit.teachsmile.app.Widget;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 public class TeachSmileWidgetService extends RemoteViewsService {
     @Override
-    public RemoteViewsFactory onGetViewFactory()
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return new ListRemoteFactory(this.getApplicationContext(), intent);
+    }
+
+    class ListRemoteFactory implements RemoteViewsService.RemoteViewsFactory {
+
+        ListRemoteFactory(Context context, Intent intent) {
+
+        }
+
+        @Override
+        public void onCreate() {
+
+        }
+
+        @Override
+        public void onDataSetChanged() {
+
+        }
+
+        @Override
+        public void onDestroy() {
+
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public RemoteViews getViewAt(int i) {
+            return null;
+        }
+
+        @Override
+        public RemoteViews getLoadingView() {
+            return null;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 0;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return false;
+        }
+    }
 }
