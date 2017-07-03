31481d2aa7d71ebb9d0d35f80550c2bb0571f779
Ankit Sharma <ankit@intrepid.io>
2017-07-03 14:37:58 -0400

    Widget in progress

---
+++ app/src/main/java/com/developer/ankit/teachsmile/app/Widget/TeachSmileWidgetService.java
@@ -0,0 +1,8 @@
+package com.developer.ankit.teachsmile.app.Widget;
+
+import android.widget.RemoteViewsService;
+
+public class TeachSmileWidgetService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory()
+}
