5c7e9810c693fdad6857f11eeac890308353b01d
Ankit Sharma <ankit@intrepid.io>
2017-07-11 10:16:43 -0400

    Added database helper

--- app/src/main/java/com/developer/ankit/teachsmile/app/data/DatabaseContract.java
+++ app/src/main/java/com/developer/ankit/teachsmile/app/data/DatabaseContract.java
@@ -13,10 +13,10 @@ public final class DatabaseContract {
         public static final String COLUMN_NAME_IMAGE_PATH = "path";
     }
 
-    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + DatabaseEntry.TABLE_NAME +
+    public static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + DatabaseEntry.TABLE_NAME +
             " (" + DatabaseEntry._ID + " INTEGER PRIMARY KEY," + DatabaseEntry.COLUMN_NAME_IMAGE_NAME
             + " TEXT," + DatabaseEntry.COLUMN_NAME_EMOTION + " TEXT," + DatabaseEntry.COLUMN_NAME_IMAGE_PATH
             + " TEXT)";
 
-    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + DatabaseEntry.TABLE_NAME;
+    public static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + DatabaseEntry.TABLE_NAME;
 }
