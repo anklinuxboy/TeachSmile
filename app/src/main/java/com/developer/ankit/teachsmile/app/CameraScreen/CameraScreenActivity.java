93520e5d9f5cc961c250fbc0f645cc27c47e0c5e
Ankit Sharma <ankit@intrepid.io>
2017-07-11 10:10:45 -0400

    Added DB contract

--- app/src/main/java/com/developer/ankit/teachsmile/app/CameraScreen/CameraScreenActivity.java
+++ app/src/main/java/com/developer/ankit/teachsmile/app/CameraScreen/CameraScreenActivity.java
@@ -1,8 +1,4 @@
 package com.developer.ankit.teachsmile.app.CameraScreen;
 
-/**
- * Created by ankit on 4/29/17.
- */
-
 public class CameraScreenActivity {
 }
---
+++ app/src/main/java/com/developer/ankit/teachsmile/app/data/DatabaseContract.java
@@ -0,0 +1,22 @@
+package com.developer.ankit.teachsmile.app.data;
+
+import android.provider.BaseColumns;
+
+public final class DatabaseContract {
+
+    private DatabaseContract() {}
+
+    public static class DatabaseEntry implements BaseColumns {
+        public static final String TABLE_NAME = "entries";
+        public static final String COLUMN_NAME_IMAGE_NAME = "name";
+        public static final String COLUMN_NAME_EMOTION = "emotion";
+        public static final String COLUMN_NAME_IMAGE_PATH = "path";
+    }
+
+    private static final String SQL_CREATE_ENTRIES = "CREATE TABLE " + DatabaseEntry.TABLE_NAME +
+            " (" + DatabaseEntry._ID + " INTEGER PRIMARY KEY," + DatabaseEntry.COLUMN_NAME_IMAGE_NAME
+            + " TEXT," + DatabaseEntry.COLUMN_NAME_EMOTION + " TEXT," + DatabaseEntry.COLUMN_NAME_IMAGE_PATH
+            + " TEXT)";
+
+    private static final String SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS " + DatabaseEntry.TABLE_NAME;
+}
